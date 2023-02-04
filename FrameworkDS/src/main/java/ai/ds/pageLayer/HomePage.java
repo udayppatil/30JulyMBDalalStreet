fb61632398a0f4ae5b51f28c9bc106ac406859c7
udayppatil <udaypatil426@gmail.com>
2023-02-05 00:41:19 +0530

    Home page task is done
    ok

--- FrameworkDS/src/main/java/ai/ds/pageLayer/HomePage.java
+++ FrameworkDS/src/main/java/ai/ds/pageLayer/HomePage.java
@@ -2,9 +2,9 @@ package ai.ds.pageLayer;
 
 public class HomePage {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println("This is Home Page");
-
 	}
 
 }
